83cf6406e67ae0e024b495060b5b65dd60ed34d1
floyd-sk <floyd.skakane@gmail.com>
2021-11-17 08:07:12 +0200

    Random Forest

--- utils/train_model.py
+++ utils/train_model.py
@@ -12,7 +12,8 @@
 # Dependencies
 import pandas as pd
 import pickle
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
 # Fetch training data and preprocess for modeling
 train = pd.read_csv('./data/df_train.csv')
@@ -21,11 +22,15 @@ y_train = train[['load_shortfall_3h']]
 X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
 
 # Fit model
-lm_regression = LinearRegression(normalize=True)
+"""lm_regression = LinearRegression(normalize=True)
 print ("Training Model...")
 lm_regression.fit(X_train, y_train)
+"""
+randomfor =RandomForestRegressor(max_depth=4, max_features="sqrt")
+print ("Training Model...")
+randomfor = randomfor.fit(train_X,train_y)
 
 # Pickle model for use within our API
-save_path = '../assets/trained-models/load_shortfall_simple_lm_regression.pkl'
+save_path = '../assets/trained-models/randomfor_model.pkl'
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(lm_regression, open(save_path,'wb'))
+pickle.dump(randomfor_model, open(save_path,'wb'))
